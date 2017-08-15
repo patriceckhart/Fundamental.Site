4ec624fa85a59430ae4ff52470e9514bbd1f3df8
Patric Eckhart <mail@patriceckhart.com>
2017-08-15 23:31:46 +0200

    no message

--- Classes/Controller/NewsController.php
+++ Classes/Controller/NewsController.php
@@ -25,11 +25,17 @@ class NewsController extends ActionController
      */
     public function indexAction() {
         $newsarticles = $this->request->getInternalArgument('__newsarticles');
+        $showdate = $this->request->getInternalArgument('__showDate');
         $nodeident = $this->request->getInternalArgument('__nodeident');
         $pagebrowser = $this->request->getInternalArgument('__pagebrowser');
+        $attributes = $this->request->getInternalArgument('__attributes');
 
-        $this->view->assign('nodeident', $nodeident);
         $this->view->assign('pageBrowser', $pagebrowser);
+        $this->view->assign('showDate', $showdate);
+
+        if ($attributes!="") {
+            $this->view->assign('attributes', ' class="' . $attributes . '"');
+        }
 
         $workspaceName = "live";
 
@@ -41,74 +47,76 @@ class NewsController extends ActionController
 
         $context = $this->contextFactory->create(array('workspaceName' => $workspaceName));
 
-        /*if ($nodeident=="") {
-
-            $node = $context->getNodeByIdentifier('b557193f-e818-4792-b2cc-051795d04d25');
+        if ($nodeident==null) {
+            $this->view->assign('noIdent', '1');
         } else {
+            $nodeident = $nodeident->getIdentifier();
+
             $node = $context->getNodeByIdentifier($nodeident);
-        }*/
 
-        $node = $context->getNodeByIdentifier($nodeident);
 
-        $articles = (new FlowQuery(array($node)))->children('[instanceof Fundamental.Site:NewsPage]')->context(array('workspaceName' => 'live'))->get();
+            $articles = (new FlowQuery(array($node)))->children('[instanceof Fundamental.Site:NewsPage]')->context(array('workspaceName' => 'live'))->get();
 
-        $pathsegment = $node->getProperty('uriPathSegment');
-        $this->view->assign('pathsegment', $pathsegment);
+            $pathsegment = $node->getProperty('uriPathSegment');
+            $this->view->assign('pathsegment', $pathsegment);
 
-        if ($this->request->hasArgument('page')) {
-            $page = $this->request->getArgument('page');
-        } else {
-            $page = 1;
-        }
-
-        $resultsCount = count($articles);
-        if ($resultsCount <= $itemsPerPage) {
-            $articles2 = $articles;
-        } else {
-            $queryOffset = $itemsPerPage * ($page - 1);
-            $queryItems = $itemsPerPage * $page;
-
-            $this->view->assign('queryOffset', $queryOffset);
-
-            $articles2 = (new FlowQuery(array($node)))->children('[instanceof Fundamental.Site:NewsPage]')->context(array('workspaceName' => 'live'))->sort('_index', 'DESC')->slice($queryOffset, $queryItems)->get();
-
-            $pages = ceil($resultsCount / $itemsPerPage);
-            if ($pages <= 10) {
-                $minPagination = 1;
-                $maxPagination = $pages;
+            if ($this->request->hasArgument('page')) {
+                $page = $this->request->getArgument('page');
+            } else {
+                $page = 1;
             }
-            if ($pages > 10) {
-                $maxPagination = $page + 5;
-                if ($maxPagination > $pages) {
+
+            $resultsCount = count($articles);
+            if ($resultsCount <= $itemsPerPage) {
+                $articles2 = $articles;
+            } else {
+                $queryOffset = $itemsPerPage * ($page - 1);
+                $queryItems = $itemsPerPage * $page;
+
+                $this->view->assign('queryOffset', $queryOffset);
+
+                $articles2 = (new FlowQuery(array($node)))->children('[instanceof Fundamental.Site:NewsPage]')->context(array('workspaceName' => 'live'))->sort('_index', 'DESC')->slice($queryOffset, $queryItems)->get();
+
+                $pages = ceil($resultsCount / $itemsPerPage);
+                if ($pages <= 10) {
+                    $minPagination = 1;
                     $maxPagination = $pages;
                 }
-                $minPagination = $maxPagination - 11;
-                if ($minPagination < 1) {
-                    $minPagination = 1;
-                    $maxPagination = 11;
+                if ($pages > 10) {
+                    $maxPagination = $page + 5;
+                    if ($maxPagination > $pages) {
+                        $maxPagination = $pages;
+                    }
+                    $minPagination = $maxPagination - 11;
+                    if ($minPagination < 1) {
+                        $minPagination = 1;
+                        $maxPagination = 11;
+                    }
+                }
+                for ($i = $minPagination; $i <= $maxPagination; $i++) {
+                    $pagination['pages'][$i]['text'] = $i;
+                }
+                if ($page > 1) {
+                    $pagination['prev'] = $page - 1;
+                }
+                if ($page < $pages) {
+                    $pagination['next'] = $page + 1;
+                }
+                if ($minPagination > 1) {
+                    $pagination['first'] = 1;
+                }
+                if ($maxPagination < $pages) {
+                    $pagination['last'] = $pages;
                 }
             }
-            for ($i = $minPagination; $i <= $maxPagination; $i++) {
-                $pagination['pages'][$i]['text'] = $i;
-            }
-            if ($page > 1) {
-                $pagination['prev'] = $page - 1;
-            }
-            if ($page < $pages) {
-                $pagination['next'] = $page + 1;
-            }
-            if ($minPagination > 1) {
-                $pagination['first'] = 1;
-            }
-            if ($maxPagination < $pages) {
-                $pagination['last'] = $pages;
-            }
+
+            $pagination['current'] = $page;
+            $this->view->assign('pagination', $pagination);
+
+            $this->view->assign('items', $articles2);
         }
 
-        $pagination['current'] = $page;
-        $this->view->assign('pagination', $pagination);
 
-        $this->view->assign('items', $articles2);
 
     }
 
